af58a8cdf784100d6260b52771caaf7cc4393161
RomanMamondez <112417045+Romannn10@users.noreply.github.com>
2023-04-13 11:59:43 -0300

    Add files via upload

---
+++ pryEDPrimerProyecto/frmCola.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEDPrimerProyecto
+{
+    public partial class frmCola : Form
+    {
+        public frmCola()
+        {
+            InitializeComponent();
+        }
+        clsCola FilaDePersonas = new clsCola();
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (FilaDePersonas.Primero != null)
+            {
+                txtCodigo2.Text = FilaDePersonas.Primero.Codigo.ToString();
+                txtNombre2.Text = FilaDePersonas.Primero.Nombre;
+                txtTramite2.Text = FilaDePersonas.Primero.Tramite;
+
+                FilaDePersonas.Eliminar();
+                FilaDePersonas.Recorrer(grlMostrar);
+                FilaDePersonas.Recorrer(lstListado);
+
+
+            }
+            else
+            {
+                txtCodigo2.Text = "";
+                txtNombre2.Text = "";
+                txtTramite2.Text = "";
+            }
+        }
+        
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Nodo objnodo = new Nodo();
+            objnodo.Codigo = Convert.ToInt32(txtCodigo.Text);
+            objnodo.Nombre = txtNombre.Text;
+            objnodo.Tramite = txtTramite.Text;
+
+            FilaDePersonas.Agregar(objnodo);
+            FilaDePersonas.Recorrer(grlMostrar);
+            FilaDePersonas.Recorrer(lstListado);
+
+            txtCodigo.Text = "";
+            txtNombre.Text = "";
+            txtTramite.Text = "";
+        }
+
+        private void txtCodigo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
