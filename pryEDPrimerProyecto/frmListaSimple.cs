4fd0e1b23b9a5ee678f4b659a5fdb7dae2569a32
RomanMamondez <112417045+Romannn10@users.noreply.github.com>
2023-04-20 12:16:08 -0300

    Add files via upload

---
+++ pryEDPrimerProyecto/frmListaSimple.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEDPrimerProyecto
+{
+    public partial class frmListaSimple : Form
+    {
+        clsListaSimple Lista = new clsListaSimple();
+        public frmListaSimple()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Nodo objNodo = new Nodo();
+            objNodo.Codigo = Convert.ToInt32(txtCodigo.Text);
+            objNodo.Nombre = txtNombre.Text;
+            objNodo.Tramite = txtTramite.Text;
+
+            Lista.Agregar(objNodo);
+            Lista.Recorrer(grlMostrar);
+            Lista.Recorrer(lstListado);
+            Lista.Recorrer(lstCodigo);
+            
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            
+
+            clsListaSimple objLista = new clsListaSimple();
+            
+            if (Lista.Primero!=null)
+            {
+                Int32 cod = Convert.ToInt32(lstCodigo.Text);
+                Lista.Eliminar(cod);
+                Lista.Recorrer(grlMostrar);
+                Lista.Recorrer(lstListado);
+                Lista.Recorrer(lstCodigo);
+            }
+            else
+            {
+                MessageBox.Show("La lista esta vacia");
+            }
+
+        }
+    }
+}
