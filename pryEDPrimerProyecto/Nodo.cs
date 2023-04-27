359e1e5617a01a9906a3f60fc67df18e1e6075e9
RomanMamondez <112417045+Romannn10@users.noreply.github.com>
2023-04-27 11:48:07 -0300

    Add files via upload

---
+++ pryEDPrimerProyecto/Nodo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryEDPrimerProyecto
+{
+    internal class Nodo
+    {
+        private Int32 cod;
+        private string Nom;
+        private string Tram;
+        private Nodo sig;
+        private Nodo ant;
+
+        public Int32 Codigo 
+        {
+            get { return cod; }
+            set { cod = value; }
+        }
+        public string Nombre
+        { 
+            get { return Nom; }
+            set { Nom = value; }
+        }
+        public string Tramite
+        {
+            get { return Tram; }
+            set { Tram = value; }
+        }
+        public Nodo Siguiente
+        {
+            get { return sig; }
+            set { sig = value; }
+        }
+        public Nodo anterior 
+        {
+            get { return ant; }
+            set { ant = value; }
+        }
+    }
+}
---
+++ pryEDPrimerProyecto/frmEstructuraDeDatos.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEDPrimerProyecto
+{
+    public partial class frmEstructuraDeDatos : Form
+    {
+        public frmEstructuraDeDatos()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void datosProgramadorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmDatosProgramador objDatosProgramador = new frmDatosProgramador();
+            objDatosProgramador.ShowDialog();
+        }
+
+        private void pilaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmPila objPila = new frmPila();
+            objPila.ShowDialog();
+        }
+
+        private void colaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCola frmCola = new frmCola();
+            frmCola.ShowDialog();
+        }
+
+        private void simpleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmListaSimple objListaSimple = new frmListaSimple();
+            objListaSimple.ShowDialog();
+        }
+
+        private void dobleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmListaDoble objListaDoble = new frmListaDoble();
+            objListaDoble.ShowDialog();
+           
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
