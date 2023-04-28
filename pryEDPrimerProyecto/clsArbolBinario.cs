d05af47259227702335ad80ad25c0cc1e85bd12e
RomanMamondez <112417045+Romannn10@users.noreply.github.com>
2023-04-28 11:45:52 -0300

    Add files via upload

---
+++ pryEDPrimerProyecto/clsArbolBinario.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEDPrimerProyecto
+{
+    internal class clsArbolBinario
+    {
+        private Nodo Inicio;
+        public Nodo Raiz 
+        {
+            get { return Inicio; }
+            set
+            {
+                Inicio = value;
+            }
+        }
+
+
+        //RECORRER LISTA
+        public void Recorrer(ListBox Lista) 
+        {
+            Lista.Items.Clear();
+            InOrdenAsc(Lista, Raiz);
+            
+        }
+        public void InOrdenAsc(ListBox lst, Nodo R) 
+        {
+            if (R.Izquierdo != null) InOrdenAsc(lst, R.Izquierdo);  //I
+            lst.Items.Add(R.Codigo); //R
+            if (R.Derecho != null) InOrdenAsc(lst, R.Derecho); //D
+            
+        }
+        public void InOrdenDes(ListBox lst, Nodo R)
+        {
+            if (R.Derecho != null) InOrdenAsc(lst, R.Derecho); //D
+            lst.Items.Add(R.Codigo); //R
+            if (R.Izquierdo != null) InOrdenAsc(lst, R.Izquierdo);  //I
+
+        }
+        public void PreOrden(ListBox lst, Nodo R)
+        {
+            lst.Items.Add(R.Codigo); //R
+            if (R.Izquierdo != null) InOrdenAsc(lst, R.Izquierdo);  //I
+            if (R.Derecho != null) InOrdenAsc(lst, R.Derecho); //D
+
+        }
+        public void PostOrden(ListBox lst, Nodo R)
+        {
+            if (R.Izquierdo != null) InOrdenAsc(lst, R.Izquierdo);  //I
+            if (R.Derecho != null) InOrdenAsc(lst, R.Derecho); //D
+            lst.Items.Add(R.Codigo); //R
+
+        }
+
+        //RECORRER COMBO
+        public void Recorrer(ComboBox Combo)
+        {
+            Combo.Items.Clear();
+            InOrdenAsc(Combo, Raiz);
+
+        }
+        public void InOrdenAsc(ComboBox cmb, Nodo R)
+        {
+            if (R.Izquierdo != null) InOrdenAsc(cmb, R.Izquierdo);  //I
+            cmb.Items.Add(R.Codigo); //R
+            if (R.Derecho != null) InOrdenAsc(cmb, R.Derecho); //D
+
+        }
+        public void InOrdenDes(ComboBox cmb, Nodo R)
+        {
+            if (R.Derecho != null) InOrdenAsc(cmb, R.Derecho); //D
+            cmb.Items.Add(R.Codigo); //R
+            if (R.Izquierdo != null) InOrdenAsc(cmb, R.Izquierdo);  //I
+
+        }
+        public void PreOrden(ComboBox cmb, Nodo R)
+        {
+            cmb.Items.Add(R.Codigo); //R
+            if (R.Izquierdo != null) InOrdenAsc(cmb, R.Izquierdo);  //I
+            if (R.Derecho != null) InOrdenAsc(cmb, R.Derecho); //D
+
+        }
+        public void PostOrden(ComboBox cmb, Nodo R)
+        {
+            if (R.Izquierdo != null) InOrdenAsc(cmb, R.Izquierdo);  //I
+            if (R.Derecho != null) InOrdenAsc(cmb, R.Derecho); //D
+            cmb.Items.Add(R.Codigo); //R
+
+        }
+
+        //RECORRER GRILLA
+
+        public void Recorrer(DataGridView Grilla)
+        {
+            Grilla.Rows.Clear();
+            InOrdenAsc(Grilla, Raiz);
+
+        }
+        public void InOrdenAsc(DataGridView Grl, Nodo R)
+        {
+            if (R.Izquierdo != null) InOrdenAsc(Grl, R.Izquierdo);  //I
+            Grl.Rows.Add(R.Codigo); //R
+            if (R.Derecho != null) InOrdenAsc(Grl, R.Derecho); //D
+
+        }
+        public void InOrdenDes(DataGridView Grl, Nodo R)
+        {
+            if (R.Derecho != null) InOrdenAsc(Grl, R.Derecho); //D
+            Grl.Rows.Add(R.Codigo); //R
+            if (R.Izquierdo != null) InOrdenAsc(Grl, R.Izquierdo);  //I
+
+        }
+        public void PreOrden(DataGridView Grl, Nodo R)
+        {
+            Grl.Rows.Add(R.Codigo); //R
+            if (R.Izquierdo != null) InOrdenAsc(Grl, R.Izquierdo);  //I
+            if (R.Derecho != null) InOrdenAsc(Grl, R.Derecho); //D
+
+        }
+        public void PostOrden(DataGridView Grl, Nodo R)
+        {
+            if (R.Izquierdo != null) InOrdenAsc(Grl, R.Izquierdo);  //I
+            if (R.Derecho != null) InOrdenAsc(Grl, R.Derecho); //D
+            Grl.Rows.Add(R.Codigo); //R
+
+        }
+
+    }
+}
---
+++ pryEDPrimerProyecto/frmArbolBinario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEDPrimerProyecto
+{
+    public partial class frmArbolBinario : Form
+    {
+        public frmArbolBinario()
+        {
+            InitializeComponent();
+        }
+
+        private void frmArbolBinario_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstListado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
